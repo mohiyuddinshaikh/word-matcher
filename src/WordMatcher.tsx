c05c8e11b7c240ee7040a2a6fd3bf76c73a7cc9c
mohiyuddin <mohiyuddin.shaikh@tradofina.com>
2024-12-31 13:23:43 +0530

    some styling and UI cues

--- src/WordMatcher.tsx
+++ src/WordMatcher.tsx
@@ -16,6 +16,9 @@ export default function WordMatcher({}: Props) {
   const [time, setTime] = useState<number>(0);
   const [currentWord, setCurrentWord] = useState<string>("");
   const [isGameOver, setIsGameOver] = useState<boolean>(false);
+  const [speed, setSpeed] = useState(1);
+
+  const MAX_SPEED = 3;
 
   useEffect(() => {
     const wordInterval = setInterval(() => {
@@ -24,7 +27,7 @@ export default function WordMatcher({}: Props) {
         return;
       }
       generateWordAndAddToBoard();
-    }, 1000);
+    }, speedOfWords());
 
     const timeInterval = setInterval(() => {
       if (isGameOver) {
@@ -38,7 +41,12 @@ export default function WordMatcher({}: Props) {
       clearInterval(wordInterval);
       clearInterval(timeInterval);
     };
-  }, [isGameOver]);
+  }, [isGameOver, speed]);
+
+  const speedOfWords = () => {
+    const temp = (MAX_SPEED + 1 - speed) * 1000;
+    return temp;
+  };
 
   function generateWordAndAddToBoard() {
     const word: string = generateRandomWord();
@@ -110,8 +118,30 @@ export default function WordMatcher({}: Props) {
         return updatedBoard;
       });
       setCurrentWord("");
-      setScore((old) => old + 1);
+      setScore((old) => {
+        const newScore = old + 1;
+        calculateSpeed(newScore);
+        return newScore;
+      });
     }
+  }
+
+  function calculateSpeed(newScore: number) {
+    if (speed === 3) {
+      return 3;
+    }
+    switch (true) {
+      case newScore > 4:
+        updateSpeed(3);
+        break;
+      case newScore > 2:
+        updateSpeed(2);
+        break;
+    }
+  }
+
+  function updateSpeed(newSpeed: number) {
+    setSpeed(newSpeed);
   }
 
   function handleAddWord(e: React.ChangeEvent<HTMLInputElement>) {
@@ -135,35 +165,49 @@ export default function WordMatcher({}: Props) {
     setIsGameOver(false);
   }
 
+  const filledBlocks = () => {
+    return board.filter((item) => item != "").length;
+  };
+
   return (
     <div className="wordMatchContainer">
-      <div className="scoreboard">
-        <div className="score">Score: {score}</div>
-        <div className="time">Time: {time}</div>
-      </div>
-      {isGameOver ? (
-        <>
-          <div className="gameOver">Game Over! Your score is {score}</div>
-          <button type="reset" onClick={resetGame}>
-            Reset
-          </button>
-        </>
-      ) : null}
-      <div className="board">
-        {board?.map((boardCell) => {
-          return <div className="boardCell">{boardCell}</div>;
-        })}
-      </div>
-      <div className="textBox">
-        <input
-          type="text"
-          name="newword"
-          onChange={handleAddWord}
-          onKeyDown={handleKeyDown}
-          value={currentWord}
-          disabled={isGameOver}
-          placeholder="Press Enter to Send"
-        />
+      <div className="gameContainer">
+        <div className="scoreboard">
+          <div className="rowOne">
+            <div className="score">Score: {score}</div>
+            <div className="time">
+              Filled: {filledBlocks()}/{board.length}
+            </div>
+          </div>
+          <div className="rowTwo">
+            <div className="time">Time: {time}</div>
+            <div className="score">Speed: {speed}x</div>
+          </div>
+        </div>
+        {isGameOver ? (
+          <>
+            <div className="gameOver">Game Over! Your score is {score}</div>
+            <button type="reset" onClick={resetGame}>
+              Reset
+            </button>
+          </>
+        ) : null}
+        <div className="board">
+          {board?.map((boardCell) => {
+            return <div className="boardCell">{boardCell}</div>;
+          })}
+        </div>
+        <div className="textBox">
+          <input
+            type="text"
+            name="newword"
+            onChange={handleAddWord}
+            onKeyDown={handleKeyDown}
+            value={currentWord}
+            disabled={isGameOver}
+            placeholder="Press Enter to Send"
+          />
+        </div>
       </div>
     </div>
   );
